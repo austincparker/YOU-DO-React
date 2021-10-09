a1bf6443bebd69712004b48a133a0ec31291acff
Austin Parker <austincparker@gmail.com>
2021-10-09 12:25:54 -0500

    finally got component to show

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,28 @@
-// import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Alert } from 'reactstrap';
 
-// export default function Todo({ todo }) {
-//     render() {
-//         return (
-//             <div>
-//                 <h1>{todo.name}</h1>
-//             </div>
-//         )
-//     }
-// }
+export default function Todo({ taco }) {
+  return (
+    <>
+      <Alert color="light">
+        <button className="btn btn-success" type="button">
+          COMPLETE
+        </button>
+        {taco.name}
+        <button className="btn btn-danger" type="button">
+          DELETE
+        </button>
+      </Alert>
+    </>
+  );
+}
 
-// Todo.proptypes = {
-//     todo: Proptypes.shape{(
-//         name: PropTypes.shape,
-//         complete: PropTypes.bool,
-//         date: PropTypes.string,
-//         uid: PropTypes.string
-//     )}.isRequired,
-// };
+Todo.propTypes = {
+  taco: PropTypes.shape({
+    name: PropTypes.string,
+    complete: PropTypes.bool,
+    date: PropTypes.string,
+    uid: PropTypes.string,
+  }).isRequired,
+};
