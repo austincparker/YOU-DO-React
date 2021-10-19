f76e88d963851a7ba82a12d2b524f007821102f5
Austin Parker <austincparker@gmail.com>
2021-10-19 18:09:06 -0500

    fixed the big auth issue

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,23 +1,50 @@
 import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { getTodos } from '../api/data/todoData';
 import TodoForm from '../components/TodoForm';
 import Navigation from '../components/Navigation';
 import Routes from '../routes';
+import SignIn from '../views/SignIn';
 
 function Initialize() {
   const [todos, setTodos] = useState([]);
   const [editItem, setEditItem] = useState({});
-
+  const [user, setUser] = useState(null);
   useEffect(() => {
-    getTodos(false).then(setTodos);
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userInfoObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0],
+        };
+        // do something
+        setUser(userInfoObj);
+        getTodos(false).then(setTodos);
+      } else if (user || user === null) {
+        setUser(false);
+        // do something else
+      }
+    });
   }, []);
 
   return (
-    <>
-      <Navigation />
-      <TodoForm obj={editItem} setTodos={setTodos} setEditItem={setEditItem} />
-      <Routes todos={todos} setTodos={setTodos} setEditItem={setEditItem} />
-    </>
+    <div>
+      {
+      user ? (
+        <>
+          <Navigation />
+          <TodoForm obj={editItem} setTodos={setTodos} setEditItem={setEditItem} />
+          <Routes todos={todos} setTodos={setTodos} setEditItem={setEditItem} />
+        </>
+      ) : (
+        <SignIn />
+      )
+}
+
+    </div>
   );
 }
 
