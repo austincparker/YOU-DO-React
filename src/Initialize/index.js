78b2bdc9f02e49fda35f3e93bdf9fb82c596b6f7
Austin Parker <austincparker@gmail.com>
2021-10-18 19:24:55 -0500

    got the sign in button

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,23 +1,51 @@
 import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
 import { getTodos } from '../api/data/todoData';
+import 'firebase/auth';
 import TodoForm from '../components/TodoForm';
 import Navigation from '../components/Navigation';
 import Routes from '../routes';
+import SignIn from '../views/SignIn';
 
 function Initialize() {
   const [todos, setTodos] = useState([]);
   const [editItem, setEditItem] = useState({});
+  const [user, setUser] = useState({});
 
   useEffect(() => {
-    getTodos(false).then(setTodos);
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        // do something
+        const userInfoObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+        };
+        setUser(userInfoObj);
+        getTodos().then(setTodos);
+      } else if (user || user === null) {
+        // do something else
+        setUser(false);
+      }
+    });
   }, []);
 
   return (
-    <>
-      <Navigation />
-      <TodoForm obj={editItem} setTodos={setTodos} setEditItem={setEditItem} />
-      <Routes todos={todos} setTodos={setTodos} setEditItem={setEditItem} />
-    </>
+    <div>
+      {user ? (
+        <>
+          <Navigation />
+          <TodoForm
+            obj={editItem}
+            setTodos={setTodos}
+            setEditItem={setEditItem}
+          />
+          <Routes todos={todos} setTodos={setTodos} setEditItem={setEditItem} />
+        </>
+      ) : (
+        <SignIn />
+      )}
+    </div>
   );
 }
 
